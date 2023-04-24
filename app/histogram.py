e0c9074b87b5f911b7798813b579e3bd9772ca34
Arthur Ruan <arthur123ruan@hotmail.com>
2023-04-23 22:19:33 -0300

    - Added histogram expansion

---
+++ app/histogram.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+
+class Histogram:
+
+    def expansion(self, image, L=255):
+
+        image_array = np.array(image)
+
+        h = image_array.shape[0]
+        w = image_array.shape[1]
+        r_min = np.amin(image_array)
+        r_max = np.amax(image_array)
+
+        output = np.zeros((h, w))
+
+        for i in range(h):
+            for j in range(w):
+                    r = image_array[i, j]
+                    s = round(
+                        ((r - r_min) / (r_max - r_min)) * (L - 1)
+                    )
+                    output[i, j] = s
+
+        return output
